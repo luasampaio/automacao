dafc0759daac5eab6be249dbf8e55b01b8337664
Luciana Sampaio <40549913+luasampaio@users.noreply.github.com>
2025-01-09 17:28:28 -0300

    Update edge.py

--- edge.py
+++ edge.py
@@ -4,8 +4,8 @@ import webbrowser as wb
 def webauto():
         chrome_path = '"C:\Program Files (x86)\Microsoft\Edge\Application\msedge.exe" %s'
         URLS = ("stackoverflow.com", "https://github.com/luasampaio", "gmail.com",
-                "https://adb-2013197995950192.12.azuredatabricks.net/browse?o=2013197995950192", "youtube.com",
-                "https://dev.azure.com/RDOr-Corp/IA")
+               "youtube.com",
+            
 
         for url in URLS:
                 print("Opening: " + url)
